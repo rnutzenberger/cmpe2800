3f50985dfe2611088841fe4b76d6b031d33a00fb
rnutzenberger <66440702+rnutzenberger@users.noreply.github.com>
2020-06-04 13:43:49 -0600

    Add files via upload

---
+++ Assignments/RNutzenebergerICA2/RNutzenebergerICA2/Program.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using GDIDrawer;
+using RNUtility;
+namespace RNutzenebergerICA2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //////////////////////////////////////////////////////
+            //Testing Genrange with no parameters
+            //////////////////////////////////////////////////////
+            Console.WriteLine("--------------------------------------------------------\nTesting GenRange(No Parameters)....\n--------------------------------------------------------");
+            foreach (int i in Utility.GenRange())
+            {
+                //spit out the returned value
+                Console.Write($"[{i}] ");
+            }
+            Console.WriteLine("\n\n");
+
+            //////////////////////////////////////////////////////
+            //Testing Genrange with Upper bounds
+            //////////////////////////////////////////////////////
+            Console.WriteLine("--------------------------------------------------------\nTesting GenRange(Upper Bound)....\n--------------------------------------------------------");
+            foreach (int i in Utility.GenRange(12))
+            {
+                Console.Write($"[{i}] ");
+            }
+            Console.WriteLine("\n\n");
+
+            //////////////////////////////////////////////////////
+            //Testing Genrange with upper and lower bounds
+            //////////////////////////////////////////////////////
+            Console.WriteLine("--------------------------------------------------------\nTesting GenRange(Upper and Lower Bound)....\n--------------------------------------------------------");
+            foreach (int i in Utility.GenRange(27,140))
+            {
+                Console.Write($"[{i}] ");
+            }
+            Console.WriteLine("\n\n");
+
+            //////////////////////////////////////////////////////
+            //Testing Genrange to throw and exception
+            //////////////////////////////////////////////////////
+            Console.WriteLine("--------------------------------------------------------\nTesting GenRange(Throw Exception)....\n--------------------------------------------------------");
+            try
+            {
+                foreach (int i in Utility.GenRange(200, 130))
+                {
+                    Console.Write($"[{i}] ");
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+            Console.WriteLine("\n\n");
+
+            //////////////////////////////////////////////////////
+            //Tetsing GenPass by getting ten 5-character strings
+            //////////////////////////////////////////////////////
+            Console.WriteLine("--------------------------------------------------------\nTesting GenPass....\n--------------------------------------------------------");
+            foreach (string s in Utility.GenPass().Take(10))
+            {
+                Console.Write($"[{s}] ");
+            }
+            Console.WriteLine("\n\n");
+
+            //////////////////////////////////////////////////////
+            //testing shuffle with a list of ints
+            //////////////////////////////////////////////////////
+            Console.WriteLine("--------------------------------------------------------\nTesting Shuffle....\n--------------------------------------------------------");
+            List<int> junk = new List<int>();
+            for(int i = 0; i < 10; ++i)
+            {
+                junk.Add(i);
+                Console.Write($"[{i}] ");
+            }
+            Console.WriteLine();
+
+            
+            foreach (int i in Utility.Shuffle(junk))
+            {
+                Console.Write($"[{i}] ");
+            }
+            Console.WriteLine("\n\n");
+
+            //////////////////////////////////////////////////////
+            //testing shuffle with a list of strings
+            //////////////////////////////////////////////////////
+            string[] junk2 = new[] { "Dogs", "Cats", "Birds", "Whales", "Zebras", "Elephants" };
+            foreach (string s in junk2)
+            {
+                Console.Write($"[{s}] ");
+            }
+            Console.WriteLine();
+            foreach (string s in Utility.Shuffle(junk2))
+            {
+                Console.Write($"[{s}] ");
+            }
+            Console.WriteLine("\n\n");
+
+
+            //////////////////////////////////////////////////////
+            //testing SuperSort with a random list of ints
+            //between 0 and 150
+            //////////////////////////////////////////////////////
+            Console.WriteLine("--------------------------------------------------------\nTesting SuperSort....\n--------------------------------------------------------");
+            Random rnd = new Random();
+            List<int> junk3 = new List<int>();
+            for(int i = 0; i < 15; ++i)
+            {
+                junk3.Add(rnd.Next(0, 151));
+                Console.Write($"[{junk3[i]}] ");
+            }
+            Console.WriteLine();
+            foreach(int i in Utility.SuperSort(junk3))
+            {
+                Console.Write($"[{i}] ");
+            }
+            Console.WriteLine("\n\n");
+
+            //////////////////////////////////////////////////////
+            //testing SuperSort with a Queue of Balls
+            //sorting the ball by radius
+            //////////////////////////////////////////////////////
+            Queue<Ball> junk4 = new Queue<Ball>();
+            for (int i = 0; i < 15; ++i)
+            {
+                junk4.Enqueue(new Ball(new Point(0, rnd.Next(10, 300)), RandColor.GetColor()));
+            }
+            foreach(Ball b in junk4)
+            {
+                Console.Write($"[{b._ballRad}] ");
+            }
+            Console.WriteLine();
+            foreach (Ball b in Utility.SuperSort(junk4))
+            {
+                Console.Write($"[{b._ballRad}] ");
+            }
+            Console.WriteLine("\n\n");
+
+            //////////////////////////////////////////////////////
+            //testing SuperSort a string and sort the chars
+            //////////////////////////////////////////////////////
+            string stuff = "SortMe!";
+            Console.Write($"{stuff}\n");
+            foreach (char c in Utility.SuperSort(stuff))
+            {
+                Console.Write($"{c}");
+            }
+            Console.ReadKey();
+        }
+
+       
+    }
+    
+}
---
+++ Assignments/RNutzenebergerICA2/RNutzenebergerICA2/Shape.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using GDIDrawer;
+
+
+namespace RNutzenebergerICA2 
+{
+    public class Ball : IComparable
+    {
+        private static Random _rnd = new Random();
+        private Point _ballLoc;
+        private int _xVel;
+        private int _yVel;
+        public int _ballRad;
+        public Color BallColor { get; set; }
+
+        public Ball(Point ballLoc, Color ballColor)
+        {
+            _xVel = _rnd.Next(-5, 6); // -5  to 5 range? 10, 0-10 set offset ? -5
+            _yVel = _rnd.Next(-5, 6);
+            _ballRad = _rnd.Next(20, 51);
+            _ballLoc = ballLoc;
+            BallColor = ballColor;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Ball arg))
+            {
+                return false;
+            }
+
+            return (CheckDiff(this, arg) < _ballRad + arg._ballRad);
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (!(obj is Ball arg))
+            {
+                throw new Exception("Object is not a Block.");
+            }
+            return _ballRad.CompareTo(arg._ballRad);
+
+        }
+
+        public override int GetHashCode()
+        {
+            return 1;
+        }
+
+        private static double CheckDiff(Ball _ballOne, Ball _ballTwo)
+        {
+            double _diffX = _ballTwo._ballLoc.X - _ballOne._ballLoc.X;
+            double _diffY = _ballTwo._ballLoc.Y - _ballOne._ballLoc.Y;
+            double _diffZ = Math.Sqrt(Math.Pow(_diffX, 2) + Math.Pow(_diffY, 2));
+            return _diffZ;
+        }
+
+        public void Move(CDrawer _canvas)
+        {
+
+            // conditions to make sure the ball does not pass throught the window extents left and right
+            //if the ball's x coordinate is less than 40(the radius) or greater than 760(the extent minus radius)
+            //then flip the sign of the current x velocity
+            if (_ballLoc.X <= _ballRad || _ballLoc.X >= _canvas.ScaledWidth - _ballRad)
+            {
+
+                _xVel *= -1;
+            }
+
+
+            //conditions to make sure the ball does not pass throught the window extents up and down
+            //if the ball's y coordinate is less than 40(the radius) or greater than 560(the extent minus radius)
+            //then flip the sign of the current y velocity
+            if (_ballLoc.Y <= _ballRad || _ballLoc.Y >= _canvas.ScaledHeight - _ballRad)
+            {
+
+                _yVel *= -1;
+            }
+
+            //the centre position of the ball will increment by the x and y velocities
+            _ballLoc.X += _xVel;
+            _ballLoc.Y += _yVel;
+
+            //check if both the x and y positions after incrementing are prone to clipping past the 
+            //extents of the window. So if the x or y positions are EX) 30, that would result in it clipping past.
+            //If it has potential of clipping past, then make the x and y positions equal to the radius of the ball
+            //or the canvas width minus the ball radius, depending on where the ball is hitting the the canvas
+            if (_ballLoc.X < _ballRad)
+            {
+                _ballLoc.X = _ballRad;
+            }
+            if (_ballLoc.X > _canvas.ScaledWidth - _ballRad)
+            {
+                _ballLoc.X = _canvas.ScaledWidth - _ballRad;
+            }
+
+            if (_ballLoc.Y < _ballRad)
+            {
+                _ballLoc.Y = _ballRad;
+            }
+            if (_ballLoc.Y > _canvas.ScaledHeight - _ballRad)
+            {
+                _ballLoc.Y = _canvas.ScaledHeight - _ballRad;
+            }
+
+
+        }
+
+        public void Show(CDrawer _canvas, int _ballNum)
+        {
+            Color comp = Color.FromArgb(BallColor.ToArgb() ^ 0x00FFFFFF);
+            _canvas.AddCenteredEllipse((int)_ballLoc.X, (int)_ballLoc.Y, _ballRad * 2, _ballRad * 2, BallColor);
+            _canvas.AddText(_ballNum.ToString(), 14, (int)_ballLoc.X - _ballRad, (int)_ballLoc.Y - _ballRad, _ballRad * 2, _ballRad * 2, comp);
+        }
+    }
+}
